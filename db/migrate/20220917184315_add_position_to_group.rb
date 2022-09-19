c6b686020b4ad77dfbfa5d99f85708e022900d09
butzhang <ke.zhang.rock@gmail.com>
2022-09-18 21:27:22 -0700

    fix migration

--- db/migrate/20220917184315_add_position_to_group.rb
+++ db/migrate/20220917184315_add_position_to_group.rb
@@ -3,8 +3,10 @@
 class AddPositionToGroup < ActiveRecord::Migration[7.0]
   def change
     add_column :groups, :position, :integer
-    Group.order(:updated_at).each.with_index(1) do |group, index|
-      group.update_column :position, index
+    ActsAsTenant.without_tenant do
+      Group.order(:updated_at).each.with_index(1) do |group, index|
+        group.update_column :position, index
+      end
     end
   end
 end
