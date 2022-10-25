b91ab8b7b173951a0577fa3db96abf96a4e24473
butzhang <ke.zhang.rock@gmail.com>
2022-10-24 18:42:30 -0700

    fix root

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 class DashboardController < ApplicationController
+  include ActionEventsHelper
   def show
-    @action_events = ActionEvent.all
-    render "action_events/index", locals: {action_events: @action_events}
+    @action_events = action_events_in_order_until(30.days.from_now)
+    @event_template_collections = EventTemplateCollection.all
+    render "action_events/index", locals: { action_events: @action_events,
+                                            event_template_collections: @event_template_collections }
   end
 end
