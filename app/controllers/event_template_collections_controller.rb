689fbb83874ac53078f641ddd893d91742a5909c
butzhang <ke.zhang.rock@gmail.com>
2022-10-24 18:33:24 -0700

    mobile ui optimization for template

--- app/controllers/event_template_collections_controller.rb
+++ app/controllers/event_template_collections_controller.rb
@@ -55,7 +55,7 @@ class EventTemplateCollectionsController < ApplicationController
     @event_template_collection.destroy
 
     respond_to do |format|
-      format.html { redirect_to(event_template_collections_path, notice: "Template was successfully destroyed.") }
+      format.html { redirect_to(action_events_path, notice: "Template was successfully destroyed.") }
       format.json { head(:no_content) }
     end
   end
