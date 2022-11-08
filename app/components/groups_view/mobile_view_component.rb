6b935ff3e59999dbdd096ddd88517d65433befcf
butzhang <ke.zhang.rock@gmail.com>
2022-11-07 19:58:44 -0800

    add pagination for dogs

--- app/components/groups_view/mobile_view_component.rb
+++ app/components/groups_view/mobile_view_component.rb
@@ -3,9 +3,11 @@
 module GroupsView
   class MobileViewComponent < ViewComponent::Base
     include Ransack::Helpers::FormHelper
+    include Pagy::Frontend
 
-    def initialize(dogs:, q:)
+    def initialize(dogs:, q:, pagy:)
       @dogs = dogs
+      @pagy = pagy
       @q = q
       @group_with_dogs = @dogs.map { |d| [d.group, d] }
                               .each_with_object({}) { |(k, v), h| h.update(k => [v]) { |_, o, n| o + n } }
