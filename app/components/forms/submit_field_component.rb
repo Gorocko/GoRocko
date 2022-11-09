30e852f8e172bb60806b5b92bbfcbf3a1e478897
butzhang <ke.zhang.rock@gmail.com>
2022-11-08 21:42:49 -0800

    add dog sort

--- app/components/forms/submit_field_component.rb
+++ app/components/forms/submit_field_component.rb
@@ -2,9 +2,10 @@
 
 module Forms
   class SubmitFieldComponent < ViewComponent::Base
-    def initialize(form:, button_override_text: nil)
+    def initialize(form:, button_override_text: nil, css_config_override: nil)
       @form = form
       @button_override_text = button_override_text
+      @css_config_override = css_config_override
     end
   end
 end
